eb3959194afc45d6511673f8b0460e520715807e
Jagwant Sehgal <sehgal.jagwant@gmail.com>
2020-06-04 18:28:35 -0400

    Updates

---
+++ precache-manifest.79ca51dc0a4457b5ef645dec89d93250.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92eeab55cd68e093de568c1ba6dcce28",
+    "url": "/personal_resume/index.html"
+  },
+  {
+    "revision": "b0c47c007156c6e52b92",
+    "url": "/personal_resume/static/css/main.b0a9ba46.chunk.css"
+  },
+  {
+    "revision": "25fe919779df066bdba9",
+    "url": "/personal_resume/static/js/2.8119b4e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal_resume/static/js/2.8119b4e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0c47c007156c6e52b92",
+    "url": "/personal_resume/static/js/main.5d94c8d8.chunk.js"
+  },
+  {
+    "revision": "890c65ec295fbea9e58a",
+    "url": "/personal_resume/static/js/runtime-main.5cc13d2c.js"
+  },
+  {
+    "revision": "fdff4014b22f67787c176c6aa52890a7",
+    "url": "/personal_resume/static/media/aws.fdff4014.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/personal_resume/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "bcf22073c9f13bab1761b90bce9fec55",
+    "url": "/personal_resume/static/media/django.bcf22073.svg"
+  },
+  {
+    "revision": "711830739c199f88a10186a11a2a1409",
+    "url": "/personal_resume/static/media/docker.71183073.svg"
+  },
+  {
+    "revision": "096438488dba28989763c9086ba3dc47",
+    "url": "/personal_resume/static/media/gcp.09643848.svg"
+  },
+  {
+    "revision": "0ce6aade3449578c0cde408e0c5c4294",
+    "url": "/personal_resume/static/media/home-image.0ce6aade.jpg"
+  },
+  {
+    "revision": "f0361c5e9bebc1c5647523db385641cf",
+    "url": "/personal_resume/static/media/html5.f0361c5e.svg"
+  },
+  {
+    "revision": "bcfad0f2c3e9c170d9e85e09a061d650",
+    "url": "/personal_resume/static/media/java.bcfad0f2.svg"
+  },
+  {
+    "revision": "891a71b9a98f1d58491e1b06c4d356a2",
+    "url": "/personal_resume/static/media/js.891a71b9.svg"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/personal_resume/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "f80a91ba03fbf0e99ea6949aa3a6399e",
+    "url": "/personal_resume/static/media/mysql.f80a91ba.svg"
+  },
+  {
+    "revision": "f97df66d2d503c31ac309c9ea90a4a82",
+    "url": "/personal_resume/static/media/python.f97df66d.svg"
+  }
+]);
